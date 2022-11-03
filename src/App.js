3fc3a355c1a2e673bfed7724902dfe987b576ac4
Maula Izza Azizi <72302421+storyofhis@users.noreply.github.com>
2022-11-03 22:51:20 +0700

    feat: add server-golang

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 /* eslint-disable no-unused-vars */
 import "./App.css";
-import AuthUser from "./pages/authUser";
 
 function App() {
-  return (
-    <div className="App">
-      <AuthUser />
-    </div>
-  );
+  return <div className="App">{/* <AuthUser /> */}</div>;
 }
 
 export default App;
