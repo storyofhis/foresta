840ef704093b7508c7d8f853c99f6f57a08d93e4
Maula Izza Azizi <72302421+storyofhis@users.noreply.github.com>
2022-11-04 21:57:30 +0700

    feat: add form login and register

--- src/App.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-unused-vars */
-import "./App.css";
-
-function App() {
-  return <div className="App">{/* <AuthUser /> */}</div>;
-}
-
-export default App;
---
+++ src/Router.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./Pages/Home";
+import "./App.css";
+import Login from "./Pages/login/index";
+import Register from "./Pages/register/index";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        {/* <Route exact path="/dashboard" /> */}
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
