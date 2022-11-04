c0f8302859cc66718852205d044c69758a819337
Maula Izza Azizi <azizi.maula@gmail.com>
2022-11-04 10:38:15 +0700

    feat: initialize server

--- src/server/httpserver/controllers/base.go
+++ src/server/httpserver/controllers/base.go
@@ -1,1 +1,10 @@
 package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/storyofhis/foresta/src/server/httpserver/controllers/views"
+)
+
+func WriteJsonResponse(ctx *gin.Context, resp *views.Response) {
+	ctx.JSON(resp.Status, resp)
+}
--- src/server/httpserver/controllers/params/userParams.go
+++ src/server/httpserver/controllers/params/userParams.go
@@ -1,1 +1,17 @@
 package params
+
+type Register struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password"`
+	Username string `json:"username"`
+}
+
+type Login struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password"`
+}
+
+type UpdateUser struct {
+	Email    string `json:"email" validate:"required,email"`
+	Username string `json:"username"`
+}
